e8a207eca0167ab911570c7f2e39eb40c1d1fe29
sankalp kataria <sankalpkataria@gmail.com>
2019-07-27 13:16:25 +0530

    added file search method

---
+++ fileSearch.js
@@ -0,0 +1,5 @@
+function readFileAndSearch() {
+
+}
+
+module.exports.readFileAndSearch = readFileAndSearch;
--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+const {createReadStream, statSync, existsSync, readdirSync} = require("fs");
 const {getFilesFromDir} = require("./getFilesFromDir.js");
+const {readFileAndSearch} = require("./fileSearch.js");
+
+function fileSearch(paths, textToSearch, options) {
+    if (!paths || !paths.length) return Promise.reject("File path is required");
+    if (!(paths instanceof Array)) return Promise.reject("Path must be an array.");
+    if (!options || !(options instanceof Object)) {
+        options = {};
+    }
+
+    let allFiles = [];
+    const areFilesPresent = paths.every(p => existsSync(p));
+    if (!areFilesPresent) return Promise.reject("One of the file path doesn't exists.");
+
+    allFiles = paths.reduce((result, p) => {
+        const fileStats = statSync(p);
+        if (!fileStats.isDirectory()) return result.concat([p]);
+        const dirFiles = getFilesFromDir(p, options.recursive, true);
+        return result.concat(dirFiles);
+    }, []);
+
+    if (!allFiles.length) return Promise.reject(`No file to search. Either there are no files or files are empty`);
+
+    const promises = allFiles.map(function(path) {
+        return readFileAndSearch(path, textToSearch, options);
+    });
+    return Promise.all(promises);
+}
+
 module.exports.getFilesFromDir = getFilesFromDir;
+module.exports.fileSearch = fileSearch;
